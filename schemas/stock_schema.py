6a6580bd90543f5f87368b83a828ef38a2558536
maciej-mlynski <80536054+maciej-mlynski@users.noreply.github.com>
2025-03-11 13:52:24 +0100

    code refactor

--- schemas/stock_schema.py
+++ schemas/stock_schema.py
@@ -1,4 +1,4 @@
-from pyspark.sql.types import StructType, StructField, TimestampType, DoubleType, IntegerType
+from pyspark.sql.types import StructType, StructField, DateType, TimestampType, DoubleType, IntegerType, StringType
 
 raw_stock_schema = StructType([
     StructField("date", TimestampType(), True),
@@ -8,8 +8,6 @@ raw_stock_schema = StructType([
     StructField("close", DoubleType(), True),
     StructField("volume", IntegerType(), True)
 ])
-
-from pyspark.sql.types import StructType, StructField, DateType, TimestampType, DoubleType, IntegerType, StringType
 
 final_stock_schema = StructType([
     StructField("date", DateType(), True),
