e05c54e8ed47c2f6973b8e225bcda737eb40ac40
maciej-mlynski <80536054+maciej-mlynski@users.noreply.github.com>
2025-03-13 12:15:45 +0100

    Replacing two dbs with single one called StockDB

--- db/create_db.py
+++ db/create_db.py
@@ -9,7 +9,7 @@ def create_databases():
     Connects to the MongoDB server and creates two databases:
       - StockDataArtifacts: stores Stock Data artifacts like current row count, start date, end date last_update for given ticker
       - ETLArtifacts: stores ETL artifacts like new tickers founded, missing tickers, gaps after each new ETL run for each new day inserted
-      
+
     If a database already exists (i.e. it appears in client.list_database_names()),
     it prints a message indicating that creation is skipped. Otherwise, it creates
     the database by inserting an initial record into a 'metadata' collection.
@@ -21,20 +21,14 @@ def create_databases():
     client = MongoClient("mongodb://localhost:27017/")
 
     # Access the databases (they will be created when data is inserted)
-    stock_data_artifacts_db = client["StockDataArtifacts"]
-    etl_artifacts_db = client["ETLArtifacts"]
+    stock_data_db = client["StockDB"]
 
-    # Helper function to create dummy collection if not exists, insert dummy doc, then drop it
-    def ensure_db_exists(db):
-        if db.name in client.list_database_names():
-            print(f"Database '{db.name}' already exists, skipping creation.")
-        else:
-            # Insert an initial record into a 'metadata' collection
-            db.metadata.insert_one({"created": True, "timestamp": datetime.now()})
-            print(f"Database '{db.name}' has been created with an initial record.")
-
-    ensure_db_exists(stock_data_artifacts_db)
-    ensure_db_exists(etl_artifacts_db)
+    if stock_data_db.name in client.list_database_names():
+        print(f"Database '{stock_data_db.name}' already exists, skipping creation.")
+    else:
+        # Insert an initial record into a 'metadata' collection
+        stock_data_db.metadata.insert_one({"created": True, "timestamp": datetime.now()})
+        print(f"Database '{stock_data_db.name}' has been created with an initial record.")
 
 
 if __name__ == "__main__":
